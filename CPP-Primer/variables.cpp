c82bed95c0b312db31df15242fde71a08c0a8fb3
Nanami Kimoto <barrier-officer0z@icloud.com>
2023-10-23 16:00:36 -0700

    variables

---
+++ CPP-Primer/variables.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Nanami Kimoto on 10/21/23.
+//
+
+#include "variables.h"
+#include <iostream>
+int main() {
+    int num = 15;
+    double nums = 15.5;
+    char letters = 'n';
+    bool greaterThan = (nums > num);
+    std::cout << "let's return some data" << std::endl;
+    return greaterThan;
+};
---
+++ CPP-Primer/variables.h
@@ -0,0 +1,8 @@
+//
+// Created by Nanami Kimoto on 10/23/23.
+//
+
+#ifndef MUGICHA_VARIABLES_H
+#define MUGICHA_VARIABLES_H
+
+#endif //MUGICHA_VARIABLES_H
