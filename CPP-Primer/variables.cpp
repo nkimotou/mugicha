29cd00548311e5dcdbdfd3643fc6177615adea94
Nanami Kimoto <barrier-officer0z@icloud.com>
2023-10-23 22:01:50 -0700

    variables

--- CPP-Primer/variables.cpp
+++ CPP-Primer/variables.cpp
@@ -5,12 +5,22 @@
 #include "variables.h"
 #include <iostream>
 int main() {
+    /* Here we can declare variables and data types.
+     * We can set up strings and concatenate them.
+     */
     int num = 15;
     double nums = 15.5;
     char letters = 'n';
     bool greaterThan = (nums > num);
-    std::cout << "let's return some data. nums > num?: " << greaterThan << std::endl;
-    // output will be '1' because greaterThan is a boolean. 1 = True, 0 = False.
+    std::string str = "C++";
+    std::string t = "T";
+    std::string rue = "rue";
+    std::cout << "are nums greater than num?: " << greaterThan << std::endl;
+    // The '+=' operator appends the second string onto the end of the first.
+    t += rue;
+    std::cout << t << std::endl;
+    // Output will be '1' because greaterThan is a boolean. 1 = True, 0 = False.
     std::cout << "what char does variable 'letters' represent?: " << letters << std::endl;
+    std::cout << "what is this primer about?: " << str << std::endl;
     return 0;
 };
